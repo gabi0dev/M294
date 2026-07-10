318a748c4beb9371a59abf27fb49c69c4f1e60cb
Gabriell Prenaj <gabriell.prenaj1@hotmail.com>
2026-07-10 13:16:19 +0200

    Grundgerüst aufgesetzt

---
+++ backend/src/main/java/ch/wiss/gamesbackend/GamesbackendApplication.java
@@ -0,0 +1,12 @@
+package ch.wiss.gamesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesbackendApplication.class, args);
+    }
+}
